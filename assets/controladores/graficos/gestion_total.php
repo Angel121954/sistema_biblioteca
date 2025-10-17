53d412100a168894f496f65c2f9a3b1759e977be
Angel David Agudelo <angeldavidagudelocuartas13@gmail.com>
2025-10-17 12:48:36 -0500

    Le agregue las gráficas correspondientes. 17/0ctubre/2025

--- assets/controladores/graficos/gestion_total.php
+++ assets/controladores/graficos/gestion_total.php
@@ -5,15 +5,16 @@ $sql->conectar();
 
 $resultado = $sql->efectuarConsulta("
     SELECT 
-        COALESCE(COUNT(DISTINCT l.id_libro), 0) AS total_libros, 
-        COALESCE(COUNT(DISTINCT r.id_reserva), 0) AS total_reservas,
-        COALESCE(COUNT(DISTINCT p.id_prestamo), 0) AS total_prestamos,
-        COALESCE(COUNT(DISTINCT u.id_usuario), 0) AS total_usuarios
+    COALESCE(COUNT(DISTINCT l.id_libro), 0) AS total_libros, 
+    COALESCE(COUNT(DISTINCT r.id_reserva), 0) AS total_reservas,
+    COALESCE(COUNT(DISTINCT p.id_prestamo), 0) AS total_prestamos,
+    COALESCE(COUNT(DISTINCT u.id_usuario), 0) AS total_usuarios
     FROM usuarios u
     LEFT JOIN reservas r ON r.usuarios_id_usuario = u.id_usuario
-    LEFT JOIN prestamos p ON p.reservas_id_reserva = r.id_reserva
     LEFT JOIN reservas_has_libros rl ON rl.reservas_id_reserva = r.id_reserva 
+    LEFT JOIN prestamos p ON p.fk_reserva_has_libro = rl.id_reserva_has_libro
     LEFT JOIN libros l ON rl.libros_id_libro = l.id_libro
+    WHERE estado_usuario != 'Inactivo'
 ");
 
 $datos = $resultado->fetch_assoc();
