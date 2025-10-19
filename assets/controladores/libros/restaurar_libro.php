6955d39ad9f3a441dc990c8c985f929bd121b33b
Angel David Agudelo <angeldavidagudelocuartas13@gmail.com>
2025-10-18 22:08:06 -0500

    Algo

--- assets/controladores/libros/restaurar_libro.php
+++ assets/controladores/libros/restaurar_libro.php
@@ -4,7 +4,6 @@ require_once "../../modelos/MySQL.php";
 $sql = new MySQL();
 $sql->conectar();
 $sql->efectuarConsulta("UPDATE libros SET disponibilidad_libro = 'Disponible'
-                        WHERE disponibilidad_libro = 'Inactivo' AND
-                        cantidad_libro > 0");
+                        WHERE disponibilidad_libro = 'Inactivo'");
 echo "ok";
 $sql->desconectar();
