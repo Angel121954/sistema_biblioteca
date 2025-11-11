898f062519dad53511b7faebd81d3d958a7fc770
Angel David Agudelo <angeldavidagudelocuartas13@gmail.com>
2025-11-11 08:12:28 -0500

    Algo

--- index_graficos.php
+++ index_graficos.php
@@ -53,7 +53,7 @@ $cantidades = $cantidades_result->fetch_assoc();
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Libros</title>
+    <title>Gráficos</title>
 
     <!--FontAwesome CDN-->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" rel="stylesheet" type="text/css">
